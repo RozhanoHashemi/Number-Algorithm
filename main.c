82710c87f5eeea8ad8e2d0fbcaa38118a286fa6b
RozhanoHashemi <150001826+RozhanoHashemi@users.noreply.github.com>
2023-11-06 13:49:26 +0330

    Add files via upload
    Write a program that takes an integer from the user as input and outputs an algorithm like the following as output. (You must determine the algorithm yourself.)

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    for(int i=1;i<=(2*a);i+=2)
+    {
+
+
+for(int j=i;j<2*a;j+=2)
+
+
+
+        printf("%d",j);
+
+printf("\n");
+
+    }
+
+
+
+
+    return 0;
+}
